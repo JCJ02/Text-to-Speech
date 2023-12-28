d94100d8190ad2f117cce90cd95392e923af41be
JCJ02 <jacobe.johncarlo.02022003@gmail.com>
2023-12-28 23:38:58 +0800

    JavaScript Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const TEXTAREA = document.getElementById("textArea");
 const SPEAKBUTTON = document.getElementById("speakButton");
 
 SPEAKBUTTON.addEventListener("click", ()=>{
+    const TEXTAREA = document.getElementById("textArea");
     let speechSynthesisUtterance = new SpeechSynthesisUtterance(TEXTAREA.value);
     speechSynthesis.speak(speechSynthesisUtterance);
 });
