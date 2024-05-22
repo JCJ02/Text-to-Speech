b40986f0c251cdc44c917acf66e2fb43773bf906
JCJ02 <jacobe.johncarlo.02022003@gmail.com>
2024-05-23 03:06:05 +0800

    Make the Synth a Filipino Language

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const SPEAKBUTTON = document.getElementById("speakButton");
 SPEAKBUTTON.addEventListener("click", ()=>{
     const TEXTAREA = document.getElementById("textArea");
     let speechSynthesisUtterance = new SpeechSynthesisUtterance(TEXTAREA.value);
+    speechSynthesisUtterance.lang = "fil"; // SET THE LANGUAGE TO FILIPINO
     speechSynthesis.speak(speechSynthesisUtterance);
 });
